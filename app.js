be7b3676f49a86108b2d4ddf2b2b916b390df899
Prakhar Gupta <54628037+prakhargupta496@users.noreply.github.com>
2021-02-01 23:42:32 +0530

    Added like button and made a compose route

--- app.js
+++ app.js
@@ -16,8 +16,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+
+
+
 app.get("/",function(req,res){
     res.render("home");
+});
+
+app.get("/compose",function(req,res){
+    res.render("compose");
+});
+
+app.post("/compose",function(req,res){
+    res.redirect("/");
 });
 
 app.listen(3000, function() {
