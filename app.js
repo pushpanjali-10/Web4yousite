5e32868ad3f49484bbf1eed9efbd0900f5f40025
Prakhar Gupta <54628037+prakhargupta496@users.noreply.github.com>
2021-02-03 21:02:32 +0530

    Started the database in the project

--- app.js
+++ app.js
@@ -16,7 +16,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+mongoose.connect("mongodb://localhost:27017/web4you", {useNewUrlParser: true});
 
+const postSchema = {
+    title: String,
+    content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
 
 
 app.get("/",function(req,res){
@@ -28,6 +35,16 @@ app.get("/compose",function(req,res){
 });
 
 app.post("/compose",function(req,res){
+    const post = new Post({
+        title: req.body.postTitle,
+        content: req.body.postBody
+      });
+
+      post.save(function(err){
+        if (!err){
+            res.redirect("/");
+        }
+      });
     res.redirect("/");
 });
 
