14f059466cb183f34b5959273f614afaf6059fac
Prakhar Gupta <54628037+prakhargupta496@users.noreply.github.com>
2021-02-04 22:42:05 +0530

    Added deletion functionality

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const { identity, remove } = require("lodash");
 
 
 const app = express();
@@ -15,20 +16,24 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+// ----Startng the database----
 
 mongoose.connect("mongodb://localhost:27017/web4you", {useNewUrlParser: true});
 
 const postSchema = {
     title: String,
-    content: String
+    content: String,
+    email: String,
+    name: String
 };
 
 const Post = mongoose.model("Post", postSchema);
 
+var delID;
 
 app.get("/",function(req,res){
     Post.find({}, function(err, posts){
-        console.log(posts);
+        // console.log(posts);
         res.render("home", {
           posts: posts
           });
@@ -43,10 +48,14 @@ app.post("/compose",function(req,res){
 
     const posttitle = req.body.postTitle;
     const postcontent =  req.body.postBody;
+    const email = req.body.composerEmail
+    const name = req.body.composerName;
 
     const post = new Post({
         title: posttitle,
-        content: postcontent
+        content: postcontent,
+        email: email,
+        name: name
       });
 
       Post.findOne({title: posttitle},function(err,obj){
@@ -59,13 +68,34 @@ app.post("/compose",function(req,res){
           }else{
               res.redirect("/exists");
           }
-      });
-
-      
+      }); 
 });
 
 app.get("/exists",function(req,res){
     res.render("exists");
+});
+
+app.post("/delete",function(req,res){
+
+    delID = req.body.checkbox;
+    res.render("delCnf");
+});
+
+app.post("/delCnf",function(req,res){
+    console.log(delID);
+    const delEmail = req.body.delEmail;
+    Post.findOne({_id: delID},function(err,post){
+        if(post.email == delEmail){
+            Post.findByIdAndRemove(delID, function(err){
+                if(!err){
+                  console.log("Successfully deleted...");
+                  res.redirect("/");
+                }
+              });
+        }else{
+            console.log("Cannot Delete!");
+        }
+    });
 });
 
 app.listen(3000, function() {
